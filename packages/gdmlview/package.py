9ef33e3241e386b61dea6ad88ffa1df4aaced566
Wouter Deconinck <wdconinc@gmail.com>
2022-01-04 13:26:15 -0600

    [gdmlview] new package

---
+++ packages/gdmlview/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Gdmlview(CMakePackage):
+    """xFSimple gdml viewer based on the Geant4 libraries."""
+
+    homepage = "https://github.com/JeffersonLab/gdmlview"
+    url      = "https://github.com/JeffersonLab/gdmlview/archive/refs/tags/v0.4.tar.gz"
+    git      = "https://github.com/JeffersonLab/gdmlview.git"
+
+    maintainers = ['wdconinc']
+
+    version('main', branch='main', preferred=True)
+    version('0.4', sha256='343ce7652418317e7e22d9edf8a99f90d0f65fb564da6e601399070b1d59a3a0')
+
+    depends_on('geant4 +opengl')
